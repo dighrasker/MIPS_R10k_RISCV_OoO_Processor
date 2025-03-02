8a745cb939e1a7664f8e81a1e939a5e8b16d7b4e
Dhruv Dighrasker <dhruvdi@caen-vnc-mi07.engin.umich.edu>
2025-03-02 17:24:07 -0500

    more additions to RS

--- verilog/RS.sv
+++ verilog/RS.sv
@@ -17,9 +17,10 @@ module RS #(
     // ------- TO/FROM: ISSUE --------- //
     input  logic           [`RS_SZ-1:0] rs_data_issuing,      // bit vector of rs_data that is being issued by issue stage
     output RS_PACKET       [`RS_SZ-1:0] RS_data,              // The entire RS data 
+    output logic           [`RS_SZ-1:0] RS_valid_next,        // 1 if RS data is valid
 
     // ------- FROM: EXECUTE (BRANCH) --------- //
-    input B_MASK                        b_mm_resolve,         // b_mask_mask to resolve
+    input B_MASK_MASK                   b_mm_resolve,         // b_mask_mask to resolve
     input logic                         b_mm_mispred          // 1 if mispredict happens
 );
 
@@ -32,6 +33,10 @@ module RS #(
             .bit_array(RS_valid)
             .count_ones(rs_spots)
         );
+        for(int i = 0; i < `RS_SZ; ++i) begin
+            //squashing step
+            RS_valid_next[i] = (b_mm_mispred && (b_mm_resolve & RS_data[i].b_mask)) ? 0 : RS_valid[i];
+        end
     end
 
     always_ff @(posedge clock) begin
@@ -39,14 +44,19 @@ module RS #(
             RS_data <= '0;
             RS_valid <= '0;
         end else begin
-            for(int i = 0; i < `N; ++i){
-            if(CDB_valid[i]) begin
-                for(int j = 0; j < `RS_SZ; ++i){
-                    RS_data[j].Source1_ready <= RS_data[j].Source1 == CDB_tags[i];
-                    RS_data[j].Source2_ready <= RS_data[j].Source2 == CDB_tags[i];
-                }
-            end 
-        }
+            RS_valid <= RS_valid_next & ~(rs_data_issuing);
+            for(int i = 0; i < `RS_SZ; ++i) begin
+                //resolving mask
+                RS_data[i].b_mask <= RS_data[i].b_mask & ~(b_mm_resolve);
+            end
+            for(int i = 0; i < `N; ++i) begin
+                if(CDB_valid[i]) begin
+                    for(int j = 0; j < `RS_SZ; ++i)begin
+                        RS_data[j].Source1_ready <= RS_data[j].Source1 == CDB_tags[i];
+                        RS_data[j].Source2_ready <= RS_data[j].Source2 == CDB_tags[i];
+                    end
+                end 
+            end
         end
     end
 
